97d5a3c760926f2f5167275e5500da48b125ee2e
Florent Cuer <florentcuer@gmail.com>
2024-10-23 14:24:29 +0100

    updated date format on timesheet details page

--- src/components/TimesheetDetailsDisplay/index.jsx
+++ src/components/TimesheetDetailsDisplay/index.jsx
@@ -1,12 +1,14 @@
 export default function TimesheetDetailsDisplay({ projectID, employeeFirstName, employeeLastName, timeTaken, description, projectTimeRemaining, dateCreated }) {
 
+    const date = new Date(dateCreated).toDateString()
+
     return (
         <>
             <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-full">
                 <div className="pb-2">
                     <div className="text-slate-600">Employee: <span className="font-bold text-blue-500">{employeeFirstName} {employeeLastName}</span></div>
                     <div className="text-slate-600">Project ID: <span className="font-bold text-blue-500">{projectID}</span></div>
-                    <div className="text-slate-600">Date Created: <span className="font-bold text-blue-500">{dateCreated}</span></div>
+                    <div className="text-slate-600">Date Created: <span className="font-bold text-blue-500">{date}</span></div>
                     <div className="text-slate-600">Time Taken: <span className="font-bold text-blue-500">{timeTaken} Hours</span></div>
                     <div className="text-slate-600">Time Remaining on Project: <span className="font-bold text-blue-500">{projectTimeRemaining} Hours</span></div>
                     <div className="text-slate-600">Description:</div>
