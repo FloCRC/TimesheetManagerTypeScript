a2013272cf133d3617621f389a11a6fb3a8f0129
Florent Cuer <florentcuer@gmail.com>
2024-10-23 15:03:48 +0100

    added hover to projects and employees components

--- src/components/EmployeeDisplay/index.jsx
+++ src/components/EmployeeDisplay/index.jsx
@@ -4,7 +4,7 @@ export default function EmployeeDisplay({ employee }) {
 
     return (
         <>
-            <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12">
+            <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12 hover:shadow-inner hover:bg-blue-200">
                 <Link to={`/employees/${employee.id}`}>
                     <div className="flex justify-between">
                         <div className="text-slate-600">ID: <span className="font-bold text-blue-500">{employee.id}</span></div>
--- src/components/ProjectDisplay/index.jsx
+++ src/components/ProjectDisplay/index.jsx
@@ -4,7 +4,7 @@ export default function ProjectDisplay({ project }) {
 
     return (
         <>
-            <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12">
+            <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12 hover:shadow-inner hover:bg-blue-200">
                 <Link to={`/projects/${project.id}`}>
                     <div className="flex justify-between pb-2">
                         <div className="text-slate-600">ID: <span className="font-bold text-blue-500">{project.id}</span></div>
