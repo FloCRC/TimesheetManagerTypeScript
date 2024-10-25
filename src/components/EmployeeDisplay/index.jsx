a4dfe8133c45244caa45a3afd297136f03bc185d
Florent Cuer <florentcuer@gmail.com>
2024-10-25 13:35:36 +0100

    added small style changes

--- src/components/EmployeeDisplay/index.jsx
+++ src/components/EmployeeDisplay/index.jsx
@@ -6,10 +6,9 @@ export default function EmployeeDisplay({ employee }) {
         <>
             <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12 hover:shadow-inner hover:bg-blue-200">
                 <Link to={`/employees/${employee.id}`}>
-                    <div className="flex justify-between">
-                        <div className="text-slate-600">ID: <span className="font-bold text-blue-500">{employee.id}</span></div>
-                        <div className="text-slate-600">First Name <span className="font-bold text-blue-500">{employee.first_name}</span></div>
-                        <div className="text-slate-600">Last Name <span className="font-bold text-blue-500">{employee.last_name}</span></div>
+                    <div className="flex justify-between pb-2">
+                        <div className="text-slate-600">Employee ID: <span className="font-bold text-blue-500">{employee.id}</span></div>
+                        <div className="text-slate-600">Name <span className="font-bold text-blue-500">{employee.first_name} {employee.last_name}</span></div>
                     </div>
                 </Link>
             </div>
--- src/components/TimesheetDisplay/index.jsx
+++ src/components/TimesheetDisplay/index.jsx
@@ -7,7 +7,7 @@ export default function TimesheetDisplay({ timesheet }) {
             <div className="flex flex-col border border-slate-600 rounded bg-white my-2 p-3 w-5/12 max-[768px]:w-10/12 hover:shadow-inner hover:bg-blue-200">
                 <Link to={`/timesheets/${timesheet.id}`}>
                     <div className="flex justify-between pb-2">
-                        <div className="text-slate-600">ID: <span className="font-bold text-blue-500">{timesheet.id}</span></div>
+                        <div className="text-slate-600">Timesheet ID: <span className="font-bold text-blue-500">{timesheet.id}</span></div>
                         <div className="text-slate-600">Project ID: <span className="font-bold text-blue-500">{timesheet.project_id}</span></div>
                     </div>
                     <div className="flex justify-between pb-2">
